8fedfadb3901abc28556cdc1219a9bcad576084a
mmicek02 <micekmichael@gmail.com>
2019-07-02 08:00:54 -0500

    Updated documents single random image is displayed. Working with different loops to print multiple images to the DOM

--- main.js
+++ main.js
@@ -5,7 +5,18 @@ function getDogImage() {
     let imageNumber = userInput.val();
     fetch('https://dog.ceo/api/breeds/image/random/' + imageNumber)
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+    .then(responseJson => displayResults(responseJson));
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  //replace the existing image with the new one
+  $('.results-img').replaceWith(
+    `<img src="${responseJson.message}" class="results-img">`
+  )
+  //display the results section
+  $('.results').removeClass('hidden');
+  console.log('Ready to reveal images!');
 }
 
 function watchForm() {
